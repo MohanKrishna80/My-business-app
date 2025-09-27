f6ac1fd240224b9a88756f015baf07b42cf57388
MohanKrishna80 <mohankrishnamohankrishna28@gmail.com>
2025-09-27 16:37:48 +0530

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -43,7 +43,7 @@ export default function ContactPage() {
             required: "Email is required",
             pattern: {
               value:
-                /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+               '@',
               message: "Invalid email address",
             },
           })}
